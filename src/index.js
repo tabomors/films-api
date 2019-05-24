b33d8add522f344ee39620bb813e2d775b9f919c
Stas Morozevich <stas.morozevich@gmail.com>
2019-05-25 01:45:39 +0300

    Add heroku stuff

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.use("/films", filmsApi);
 app.use(error);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App is listening on the port ${PORT}`);
 });
